afd06c49db5e51f865f7af35addd13cb2f6567d3
nya <jianglinran@eomgames.net>
2026-03-04 17:48:07 +0800

    文件路径

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import io
 import os
 import asyncio
 from astrbot.core.utils.astrbot_path import get_astrbot_data_path
+from astrbot.api.star import StarTools
 import tempfile
 from PIL import Image as PILImage, ImageDraw, ImageFont, ImageFilter
 
@@ -15,7 +16,7 @@ class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
         self._hero_cache = None  # hero_id -> {name, localized_name}
-        self._hero_img_dir = get_astrbot_data_path() / "plugin_data" / "xx_bot" / "heroes"
+        self._hero_img_dir = StarTools.get_data_dir("xx_bot") / "heroes"
 
     async def initialize(self):
         """插件初始化：预加载英雄数据和头像缓存"""
